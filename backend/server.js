5851e9e6a10c7353c43351eaef34fb73862420a5
vikhulei <vikhulei@gmail.com>
2024-10-25 08:38:52 +0100

    server started

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("test")
+const express = require ("express")
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+app.use("/", (req, res) => {
+    res.send("Request received")
+})
