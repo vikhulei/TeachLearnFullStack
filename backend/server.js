f5d0bf7fc9a589fed2e0f217e301d84eadaf0449
vikhulei <vikhulei@gmail.com>
2024-10-26 10:44:06 +0100

    routes and .env added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require ("express")
 const app = express()
+const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
 
-app.use("/", (req, res) => {
-    res.send("Request received")
-})
+app.use("/api/users", require("./routes/userRoutes"))
