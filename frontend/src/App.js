50a01d48c06bb496d6f51d174aad8ebd767f5f62
vikhulei <vikhulei@gmail.com>
2024-10-28 08:44:51 +0000

    frontend created

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header';
+import Dashbord from './pages/Dashbord';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+        <Routes>
+          <Route path='/' element={<Dashbord/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+        </Routes>
+<ToastContainer />
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
