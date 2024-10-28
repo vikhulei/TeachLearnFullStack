13b8c3ff37a67ed903bbac1071b2fa1c1e0207e9
vikhulei <vikhulei@gmail.com>
2024-10-28 20:59:23 +0000

    app started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-import Header from './components/Header';
-import Dashbord from './pages/Dashbord';
-import Login from './pages/Login';
-import Register from './pages/Register';
+// import Header from './components/Header';
+// import Dashbord from './pages/Dashbord';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 
 const App = () => {
   return (
     <>
       <Router>
         <div className='container'>
-          <Header />
+          <h1>Hello I am here</h1>
+          {/* <Header />
         <Routes>
           <Route path='/' element={<Dashbord/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
         </Routes>
-<ToastContainer />
+<ToastContainer /> */}
         </div>
       </Router>
     </>
