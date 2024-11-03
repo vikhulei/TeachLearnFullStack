9380fa580a23b9775b40fcaae6f27b46f6433957
vikhulei <vikhulei@gmail.com>
2024-11-03 07:03:46 +0000

    svg mask completed

--- frontend/src/pages/01_Home/Home.js
+++ frontend/src/pages/01_Home/Home.js
@@ -17,11 +17,11 @@ const Home = () => {
         <defs>
           <mask id="mask">
 
-            <rect x="0" y="0" width="100%" height="100%" />
-            <circle cx="35%" cy="42%" r="20%" />
+            <rect x="0" y="0" width="100%" height="100%" fill="white" />
+            <circle cx="35%" cy="42%" r="20%" fill="black" />
           </mask>
         </defs>
-        <rect x="0" y="0" width="100%" height="100%" mask="url(#mask)" fill-opacity="0.55" />
+        <rect x="0" y="0" width="100%" height="100%" mask="url(#mask)" fill="rgba(7, 83, 91, 0.65)"  />
       </svg>
     </SvgWrapper>
 
@@ -38,3 +38,4 @@ const Home = () => {
 export default Home
 
           {/* <mask id="mask" x="0" y="0" width="80" height="30"> */}
+                      {/* <rect x="0" y="0" width="100%" height="100%" /> */}
