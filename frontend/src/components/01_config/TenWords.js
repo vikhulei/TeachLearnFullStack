741eccb031733032ae7626b6f9e34e0bf39f96af
vikhulei <vikhulei@gmail.com>
2024-11-25 06:13:54 +0000

    10 words created

---
+++ frontend/src/components/01_config/TenWords.js
@@ -0,0 +1,12 @@
+export const TenWord = {
+    521: "world",
+    522: "apple",
+    523: "their",
+    524: "also",
+    525: "if",
+    526: "that",
+    527: "or",
+    528: "people",
+    529: "as",
+    530: "on"
+}
