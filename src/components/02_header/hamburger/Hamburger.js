44f720e2e41812f59eeed826aee4a8ca5ecf9a40
vikhulei <vikhulei@gmail.com>
2025-01-29 08:12:08 +0000

    hamburger started

---
+++ src/components/02_header/hamburger/Hamburger.js
@@ -0,0 +1,12 @@
+import { HambWrapper } from "./HamburgerStyle"
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Hamburger = () => {
+  return (
+    <HambWrapper>
+        <GiHamburgerMenu />
+    </HambWrapper>
+  )
+}
+
+export default Hamburger
---
+++ src/components/02_header/hamburger/HamburgerStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const HambWrapper = styled.div`
+    position: absolute;
+    right: 90px;
+    height: 100%;
+    font-size: 30px;
+`
+
+export { HambWrapper }
