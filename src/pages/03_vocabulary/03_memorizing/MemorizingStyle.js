69fd0ac43d0336215e39737ae25b2ab228dc706d
vikhulei <vikhulei@gmail.com>
2025-02-13 20:21:14 +0000

    vocabulary-memorizing mobile started

--- src/pages/03_vocabulary/03_memorizing/MemorizingStyle.js
+++ src/pages/03_vocabulary/03_memorizing/MemorizingStyle.js
@@ -3,10 +3,16 @@ import { MainText } from "../VocabularyStyle"
 
 const MemorizingWrapper = styled.div`
     position: relative;
+    width: 100%;
     padding: 0 7vw;
     display: flex;
     justify-content: center;
     gap: 5vw;
+    @media(max-width: 800px) {
+        flex-direction: column;
+        gap: 5vh;
+        // background-color: red;
+    }
 `
 
 const CircleTextWrapper = styled.div`
@@ -19,6 +25,17 @@ const CircleTextWrapper = styled.div`
     text-align: center;
     &:nth-of-type(2) {
         top: 40px;
+    }
+    @media(max-width: 800px) {
+        justify-content: start;
+        align-items: center;
+        // background-color: green;
+        padding: 30px 0 30px 30px;
+        // padding-left: 30px;
+        &:nth-of-type(2) {
+            top: 0px;
+            padding-right: 60px;
+        }
     }
     z-index: 20;
 `
@@ -42,16 +59,26 @@ const Line = styled.div`
     height: 80px;
     border-bottom: solid #B5B5B5 3px;
     z-index: 10;
+    @media(max-width: 800px) {
+        width: calc(50% - 7vw);
+        height: 100%;
+        border-bottom: none;
+        border-right: solid #B5B5B5 3px;
+        // background-color: red;
+    }
+    // display: none;
 `
 
 const CircleHeading = styled.div`
     margin: 20px 0;
     font-size: 1.6rem;
     font-weight: bold;
+    display: none;
 `
 
 const CircleText = styled(MainText)`
     padding: 0;
+    display: none;
 `
 
 export { MemorizingWrapper, CircleTextWrapper, Circle, Line, CircleHeading, CircleText }
