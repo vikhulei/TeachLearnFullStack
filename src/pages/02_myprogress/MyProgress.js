96c64617b1acc0901ca25f415d489912366f457d
vikhulei <vikhulei@gmail.com>
2025-02-21 08:22:51 +0000

    feat(myprogress): title and main text created

--- src/pages/02_myprogress/MyProgress.js
+++ src/pages/02_myprogress/MyProgress.js
@@ -1,4 +1,4 @@
-import { Wrapper, Header } from "./MyProgressStyle"
+import { Wrapper, Header, Title, MainText } from "./MyProgressStyle"
 import home from "../../assets/01_home/home.jpg"
 
 const MyProgress = () => {
@@ -7,6 +7,10 @@ const MyProgress = () => {
       <Header>
         My Progress
       </Header>
+            <Title>My Roadmap</Title>
+            <MainText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet libero id nisi euismod, sed porta est consectetur. Vestibulum auctor felis eget orci semper vestibulum. Pellentesque ultricies nibh gravida, accumsan libero luctus, molestie nunc. In nibh ipsum, blandit id faucibus ac, finibus vitae dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet libero id nisi euismod, sed porta est consectetur. Vestibulum auctor felis eget orci semper vestibulum. Pellentesque ultricies nibh gravida, accumsan libero luctus, molestie nunc. In nibh ipsum, blandit id faucibus ac, finibus vitae dui.
+            </MainText>
     </Wrapper>
   )
 }
