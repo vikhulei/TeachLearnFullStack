c405df0571039f8d9d0fe88c1225bcdb8f7925ed
vikhulei <vikhulei@gmail.com>
2025-02-25 08:35:49 +0000

    refactor(myprogress): roadmap curveline

--- src/pages/02_myprogress/01_roadmap/RoadmapStyle.js
+++ src/pages/02_myprogress/01_roadmap/RoadmapStyle.js
@@ -5,61 +5,43 @@ const RoadmapContainer = styled.div`
   height: 500px;
   width: 870px;
   margin: 0 auto;
+  display: grid;
   // background-color: green;
 `
 
-const CurveLineRightOne = styled.div`
+const CurveLine = styled.div`
   position: absolute;
   width: 800px;
   height: 100px;
+  border: solid darkgrey 15px; 
+`
+
+const CurveLineRightOne = styled(CurveLine)`
   transform: translateX(70px);
   border-top-right-radius: 50px;
   border-bottom-right-radius: 50px;
-  border: solid darkgrey 15px; 
   border-left: none;
 `
 
-const CurveLineLeftOne = styled.div`
-  position: absolute;
-  width: 800px;
-  height: 100px;
+const CurveLineLeftOne = styled(CurveLine)`
   top: 85px;
   border-top-left-radius: 50px;
   border-bottom-left-radius: 50px;
-  border: solid darkgrey 15px; 
   border-right: none;
 `
 
-const CurveLineRightTwo = styled.div`
-  position: absolute;
-  width: 800px;
-  height: 100px;
+const CurveLineRightTwo = styled(CurveLineRightOne)`
   top: 170px;
-  transform: translateX(70px);
-  border-top-right-radius: 50px;
-  border-bottom-right-radius: 50px;
-  border: solid darkgrey 15px; 
-  border-left: none;
 `
 
-const CurveLineLeftTwo = styled.div`
-  position: absolute;
-  width: 800px;
-  height: 100px;
+const CurveLineLeftTwo = styled(CurveLineLeftOne)`
   top: 255px;
-  border-top-left-radius: 50px;
-  border-bottom-left-radius: 50px;
-  border: solid darkgrey 15px; 
-  border-right: none;
 `
 
-const Start = styled.div`
-  position: absolute;
+const Circle = styled.div`
   width: 100px;
   height: 100px;
   border-radius: 100px;
-  top: -40px;
-  left: 0;
   background-color: blue;
   z-index: 10;
   display: flex;
@@ -70,6 +52,24 @@ const Start = styled.div`
   color: white;
 `
 
-const Heading = styled.div
+const Start = styled(Circle)`
+  position: absolute;
+  top: -40px;
+  left: 0;
+`
+
+const Finish = styled(Circle)`
+  position: absolute;
+  bottom: -40px;
+  right: 0;
+`
+
+const CirclesWrapper = styled.div`
+  display: grid;
+
+
+`
+
+
 
 export { RoadmapContainer, CurveLineRightOne, CurveLineLeftOne, CurveLineRightTwo, CurveLineLeftTwo, Start }
