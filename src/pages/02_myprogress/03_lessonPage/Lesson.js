d112f36fcacb0efa63f71c413ebe808328816089
vikhulei <vikhulei@gmail.com>
2025-03-06 22:15:15 +0000

    vocabulary in lesson started

--- src/pages/02_myprogress/03_lessonPage/Lesson.js
+++ src/pages/02_myprogress/03_lessonPage/Lesson.js
@@ -1,11 +1,46 @@
-import { Wrapper, Header } from "./LessonStyle"
+import { Wrapper, Header, Heading, SubheadingCapital, VocabularyWrapper, Word } from "./LessonStyle"
+import { TwoThousand } from "../../../components/01_config/TwoThousand"
+
+const lessonParts = ["Vocabulary", "Grammar", "Reading", "Listening", "Speaking"]
 
 const Lesson = () => {
     return (
         <Wrapper>
             <Header>
-                Lesson
+                MY PROGRESS
             </Header>
+            <Heading>
+                Lesson 3
+            </Heading>
+{/* 
+            {lessonParts.map((value, index) => (
+                <SubheadingCapital key={index}>
+                    {value}
+                </SubheadingCapital>
+            ))} */}
+
+            <SubheadingCapital>
+                Vocabulary
+            </SubheadingCapital>
+            <VocabularyWrapper>
+            {TwoThousand.filter(val => val.id <= 30 ).map((value, index) => (
+                <Word key={index}>
+                    {value.word}
+                </Word>
+            ))}
+            </VocabularyWrapper>
+            <SubheadingCapital>
+                Grammar
+            </SubheadingCapital>
+            <SubheadingCapital>
+                Reading
+            </SubheadingCapital>
+            <SubheadingCapital>
+                Listening
+            </SubheadingCapital>
+            <SubheadingCapital>
+                Speaking
+            </SubheadingCapital>
         </Wrapper>
     )
 }
--- src/pages/02_myprogress/03_lessonPage/LessonStyle.js
+++ src/pages/02_myprogress/03_lessonPage/LessonStyle.js
@@ -1,12 +1,38 @@
 import styled from "styled-components";
-import { MainWrapper, MainHeader } from "../../../components/01_config/MainStyles";
+import { colors } from "../../../components/01_config/Colors";
+import { MainWrapper, MainHeader, MainHeading, MainHeadingCapital, MainBodyText } from "../../../components/01_config/MainStyles";
 
 const Wrapper = styled(MainWrapper)`
-
+    gap: 30px;
 `
 
 const Header = styled(MainHeader)`
 
 `
 
-export { Wrapper, Header }
+const Heading = styled(MainHeading)`
+    text-align: center;
+`
+
+const SubheadingCapital = styled(MainHeadingCapital)`
+    color: ${colors.myprogress}
+`
+
+const VocabularyWrapper = styled.div`
+    display: flex;
+    padding: 0 20px;
+`
+
+const Word = styled.span`
+    &:after {
+        content: ",  ";
+        white-space: pre;
+    }
+    &:last-of-type {
+        &:after {
+        content: ""
+    }
+    }
+`
+
+export { Wrapper, Header, Heading, SubheadingCapital, VocabularyWrapper, Word }
