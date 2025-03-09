70257f58c0e075985fb99a86e5fefbc73bd931ef
vikhulei <vikhulei@gmail.com>
2025-03-09 09:57:59 +0000

    BoldWords updated

--- src/pages/02_myprogress/03_lessonPage/01_exercises/BoldWords.js
+++ src/pages/02_myprogress/03_lessonPage/01_exercises/BoldWords.js
@@ -1,8 +1,8 @@
 export const BoldWords = [
-    {id: 1, lesson: 1, word: "Я", translat: "I", sentence: "love Ukraine"},
-    {id: 1, lesson: 1, word: "Ви", translat: "You", sentence: "love UK"},
-    {id: 1, lesson: 1, word: "Він", translat: "He", sentence: "loves Kyiv"},
-    {id: 2, lesson: 1, word: "Вона", translat: "She", sentence: "loves USA"},
-    {id: 3, lesson: 1, word: "Ми", translat: "We", sentence: "love London"},
+    {id: 1, lesson: 1, beginning: "", word: "Я", translat: "I", ending: "love Ukraine"},
+    {id: 1, lesson: 1, beginning: "", word: "Вони", translat: "I", ending: "love Kyiv"},
+    {id: 1, lesson: 1, beginning: "Today", word: "він", translat: "he", ending: "loves USA"},
+    {id: 1, lesson: 1, beginning: "", word: "Вона", translat: "she", ending: "loves England"},
+    {id: 1, lesson: 1, beginning: "Of course", word: "ми", translat: "we", ending: "love London"},
     
 ]
