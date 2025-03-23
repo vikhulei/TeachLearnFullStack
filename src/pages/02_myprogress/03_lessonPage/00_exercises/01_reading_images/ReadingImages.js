a152aee2883967273aaac9cb1e82d6e183751842
vikhulei <vikhulei@gmail.com>
2025-03-23 23:01:11 +0000

    reading exercises and images for exercises started

---
+++ src/pages/02_myprogress/03_lessonPage/00_exercises/01_reading_images/ReadingImages.js
@@ -0,0 +1,5 @@
+export const ReadingImages = [
+    {id: 1, lesson: 1, image: "./00_exercises/01_reading_images/home.jpg"},
+    {id: 2, lesson: 1, image: "./00_exercises/01_reading_images/student.jpg"},
+    {id: 3, lesson: 1, image: "./00_exercises/01_reading_images/tutor.jpg"},
+]
