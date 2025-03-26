f36aa12942c16befd93d517cc42e8a9497389fd5
vikhulei <vikhulei@gmail.com>
2025-03-26 08:36:01 +0000

    array created for adding images with description

--- src/pages/02_myprogress/03_lessonPage/00_exercises/01_reading_images/ReadingImages.js
+++ src/pages/02_myprogress/03_lessonPage/00_exercises/01_reading_images/ReadingImages.js
@@ -1,5 +1,36 @@
-export const ReadingImages = [
-    {id: 1, lesson: 1, image: "./00_exercises/01_reading_images/home.jpg"},
-    {id: 2, lesson: 1, image: "./00_exercises/01_reading_images/student.jpg"},
-    {id: 3, lesson: 1, image: "./00_exercises/01_reading_images/tutor.jpg"},
+import home from "./image3.jpg"
+
+// export const Images = Array.from({ length: 3 }, (_, i) => require(`./00_exercises/01_reading_images/image${i + 1}.jpg`));
+
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+
+
+const images = importAll(
+    require.context("./", false, /\.jpg$/)
+);
+
+const description = [
+    "teacher",
+    "student",
+    "home"
 ]
+
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+export const ReadingImages = images.map((image, index) => (
+    {
+        id: index+1,
+        image,
+        description: description[index]
+    }
+))
+
+// export const ReadingImages = [
+//     { id: 1, lesson: 1, image: image1, description: "cup" },
+//     { id: 2, lesson: 1, image: image2, description: "plate" },
+//     { id: 3, lesson: 1, image: image3, description: "table" },
+// ]
--- src/pages/02_myprogress/03_lessonPage/Lesson.js
+++ src/pages/02_myprogress/03_lessonPage/Lesson.js
@@ -1,20 +1,19 @@
 import { Wrapper, Header } from "./LessonStyle"
 import Vocabulary from "./01_vocabulary/Vocabulary"
 import Grammar from "./02_grammar/Grammar"
-// import { ReadingImages } from "./00_exercises/01_reading_images/ReadingImages"
 import { FrameWrapper } from "./02_grammar/01_RulesStyle"
 import { Image } from "./02_grammar/02_exercises/ExercisesStyle"
-import tutor from "./00_exercises/01_reading_images/tutor.jpg"
-import { FaRegNewspaper } from "react-icons/fa"
+import { ReadingImages } from "./00_exercises/01_reading_images/ReadingImages"
 
-const ReadingImages = [
-    {id: 1, lesson: 1, im: "./00_exercises/01_reading_images/home.jpg"},
-    {id: 2, lesson: 1, im: "./00_exercises/01_reading_images/student.jpg"},
-    {id: 3, lesson: 1, im: "./00_exercises/01_reading_images/tutor.jpg"},
-]
+const Images = Array.from({ length: 3 }, (_, i) => require(`./00_exercises/01_reading_images/image${i + 1}.jpg`));
+
+
 
 
 const Lesson = () => {
+    const clickImage = (e) => {
+        console.log(e.currentTarget.getAttribute("data-description"))
+    }
     return (
         <Wrapper>
             <Header>
@@ -22,14 +21,13 @@ const Lesson = () => {
             </Header>
             <Vocabulary />
             <Grammar />
-                <Image src={tutor}/>
-                {ReadingImages.map((value, index) => (
-                    <FrameWrapper key={index}>
+                {/* <Image src={tutor}/> */}
+                <FrameWrapper>
+                    {ReadingImages.map((value, index) => (
+                        <Image key={index} src={value.image} id={index} data-description={value.description} onClick={clickImage}/>
+                    ))}
+                </FrameWrapper>
 
-                    <Image src={value.im}/>
-                    <div>{value.id}</div>
-                    </FrameWrapper>
-                ))}
         </Wrapper>
     )
 }
