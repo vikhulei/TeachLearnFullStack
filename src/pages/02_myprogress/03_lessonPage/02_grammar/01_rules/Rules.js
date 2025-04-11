50ff02cd97671f3f9c5be2e3092763d24fc71fc1
vikhulei <vikhulei@gmail.com>
2025-04-11 08:28:53 +0100

    exercises style cleaned

---
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/01_rules/Rules.js
@@ -0,0 +1,16 @@
+import { FrameWrapper, Frame, Image} from "./RulesStyle"
+import { RulesData } from "./RulesData"
+
+const Rules = () => {
+    return (
+            <FrameWrapper>
+                {RulesData.filter(val => val.lesson === 1).map((value, index) => (
+                    <Frame key={index}>
+                        <Image src={value.image} />
+                    </Frame>
+                ))}
+            </FrameWrapper>
+    )
+}
+
+export default Rules
---
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/01_rules/RulesStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { MainFrame, MainFrameWrapper, MainImage } from "../../../../../components/01_config/MainStyles";
+
+
+const FrameWrapper = styled(MainFrameWrapper)`
+`
+
+const Frame = styled(MainFrame)`
+    height: fit-content;
+`
+
+const Image = styled(MainImage)`
+`
+
+export { FrameWrapper, Frame, Image }
