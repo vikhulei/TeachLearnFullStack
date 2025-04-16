35898e97d83aa51f83271f442eed651657981fff
vikhulei <vikhulei@gmail.com>
2025-04-16 08:03:03 +0100

    fillitinexercisedatat started!

--- src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
@@ -3,13 +3,24 @@ export const FillitinExerciseData = [
     {id: 2, lesson: 1, sentence: "Сьогодні гарна погода", translat: "The weather is great today", visibility: "hidden", input: ""},
     {id: 3, lesson: 1, sentence: "Ви дивилися цей фільм?", translat: "Have you watched that movie?", visibility: "hidden", input: ""},
     {id: 4, lesson: 1, sentence: "В понеділок вихідний", translat: "Monday is a holiday", visibility: "hidden", input: ""},
-    {id: 5, lesson: 1, sentence: "Я люблю Україну", translat: "I love Ukraine", visibility: "hidden", input: ""},
-    {id: 6, lesson: 1, sentence: "Сьогодні гарна погода", translat: "The weather is great today", visibility: "hidden", input: ""},
-    {id: 7, lesson: 1, sentence: "Ви дивилися цей фільм?", translat: "Have you watched that movie?", visibility: "hidden", input: ""},
-    {id: 8, lesson: 2, sentence: "В понеділок вихідний", translat: "Monday is a holiday", visibility: "hidden", input: ""},
-    {id: 9, lesson: 2, sentence: "Я люблю Україну", translat: "I love Ukraine", visibility: "hidden", input: ""},
-    {id: 10, lesson: 2, sentence: "Сьогодні гарна погода", translat: "The weather is great today", visibility: "hidden", input: ""},
-    {id: 11, lesson: 2, sentence: "Ви дивилися цей фільм?", translat: "Have you watched that movie?", visibility: "hidden", input: ""},
-    {id: 12, lesson: 2, sentence: "В понеділок вихідний", translat: "Monday is a holiday", visibility: "hidden", input: ""},
-
 ]
+
+const sentences = [
+    {id: 1, lesson: 1, sentence: "Я люблю Україну", translat: "I love Ukraine", visibility: "hidden", input: ""},
+    {id: 2, lesson: 1, sentence: "Сьогодні гарна погода", translat: "The weather is great today", visibility: "hidden", input: ""},
+    {id: 3, lesson: 1, sentence: "Ви дивилися цей фільм?", translat: "Have you watched that movie?", visibility: "hidden", input: ""},
+    {id: 4, lesson: 1, sentence: "В понеділок вихідний", translat: "Monday is a holiday", visibility: "hidden", input: ""},
+]
+
+const FillitinExercise = sentences.map((value, index) => (
+    {
+        id: index+1,
+        lesson: Math.floor(index/10) + 1,
+        visibility: "hidden",
+        input: "",
+        sentence: {
+            sentence: value.sentence,
+            translat: value.translat,
+        }
+    }
+))
