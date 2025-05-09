f66504be3dcfe3068ba05c514d71bdc8ffb8542a
vikhulei <vikhulei@gmail.com>
2025-05-09 08:48:08 +0100

    leson 2 completed

--- src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
@@ -13,16 +13,16 @@ const sentences = [
 
     // LESSON 2
 
-    {sentence: "M-A-K-E", translat: ""},
-    {sentence: "W-O-R-K", translat: ""},
-    {sentence: "G-O-O-D", translat: ""},
-    {sentence: "U-S-E-D", translat: ""},
-    {sentence: "O-N-L-Y", translat: ""},
-    {sentence: "L-I-K-E", translat: ""},
-    {sentence: "U-S-E-D", translat: ""},
-    {sentence: "W-A-T-E-R", translat: ""},
-    {sentence: "T-I-M-E", translat: ""},
-    {sentence: "your name", translat: ""},
+    {sentence: "Як тебе звати?", translat: "What is your name?"},
+    {sentence: "Ти не американець", translat: "You are not American"},
+    {sentence: "Вони були в місті?", translat: "Were they in the city?"},
+    {sentence: "Він з України?", translat: "Is he from Ukraine"},
+    {sentence: "Ці люди не дуже дружні", translat: "These people are not very friendly"},
+    {sentence: "Твоя робота важка?", translat: "Is your work hard?"},
+    {sentence: "Фільм нецікавий", translat: "Film is not interesting"},
+    {sentence: "Те місце гарне?", translat: "Is that place nice?"},
+    {sentence: "Сім'я важлива?", translat: "Is family important?"},
+    {sentence: "Цей бізнес нелегкий", translat: "This business is not easy"},
 
 ]
 
