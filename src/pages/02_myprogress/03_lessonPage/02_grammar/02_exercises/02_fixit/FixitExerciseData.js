4c0d62e05ff3b9e70942ddff9830137ea2dd7f57
vikhulei <vikhulei@gmail.com>
2025-05-10 13:16:38 +0100

    lesson 4 exercises completed

--- src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/02_fixit/FixitExerciseData.js
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/02_fixit/FixitExerciseData.js
@@ -1,44 +1,52 @@
 
 const sentences = [
     // LESSON 1
-    {beginning: "", bold: "do - [dou]?", correct: "ду [du:]", ending: ""},
-    {beginning: "", bold: "some - [soum]?", correct: "сам [sʌm]", ending: ""},
-    {beginning: "", bold: "people - ?", correct: "піпл [pi:pl]", ending: ""},
-    {beginning: "", bold: "see - [si:]?", correct: "сі [si:]", ending: ""},
-    {beginning: "", bold: "most - [most]?", correct: "моуст [moust]", ending: ""},
-    {beginning: "", bold: "what - [whæt]?", correct: "увот [wot]", ending: ""},
-    {beginning: "", bold: "through - ?", correct: "тсру [θru:]", ending: ""},
-    {beginning: "", bold: "also - [ælsou]?", correct: "олсоу [olsou]", ending: ""},
+    { beginning: "", bold: "do - [dou]?", correct: "ду [du:]", ending: "" },
+    { beginning: "", bold: "some - [soum]?", correct: "сам [sʌm]", ending: "" },
+    { beginning: "", bold: "people - ?", correct: "піпл [pi:pl]", ending: "" },
+    { beginning: "", bold: "see - [si:]?", correct: "сі [si:]", ending: "" },
+    { beginning: "", bold: "most - [most]?", correct: "моуст [moust]", ending: "" },
+    { beginning: "", bold: "what - [whæt]?", correct: "увот [wot]", ending: "" },
+    { beginning: "", bold: "through - ?", correct: "тсру [θru:]", ending: "" },
+    { beginning: "", bold: "also - [ælsou]?", correct: "олсоу [olsou]", ending: "" },
 
     //LESSON 2
 
-    {beginning: "I", bold: "to be (зараз)", correct: "am", ending: "a student "},
-    {beginning: "You", bold: "to be (в минулому)", correct: "were", ending: "a worker"},
-    {beginning: "He", bold: "to be (завтра)", correct: "will be", ending: "at school"},
-    {beginning: "She", bold: "to be (зараз)", correct: "is", ending: "happy"},
-    {beginning: "We", bold: "to be (вчора)", correct: "were", ending: "at home"},
-    {beginning: "Students", bold: "to be not (зараз)", correct: "are not", ending: "in the class"},
-    {beginning: "The boy", bold: "to be not (завтра)", correct: "will not be", ending: "at work"},
-    {beginning: "We", bold: "to be (зараз)", correct: "are", ending: "friends"},
+    { beginning: "I", bold: "to be (зараз)", correct: "am", ending: "a student " },
+    { beginning: "You", bold: "to be (в минулому)", correct: "were", ending: "a worker" },
+    { beginning: "He", bold: "to be (завтра)", correct: "will be", ending: "at school" },
+    { beginning: "She", bold: "to be (зараз)", correct: "is", ending: "happy" },
+    { beginning: "We", bold: "to be (вчора)", correct: "were", ending: "at home" },
+    { beginning: "Students", bold: "to be not (зараз)", correct: "are not", ending: "in the class" },
+    { beginning: "The boy", bold: "to be not (завтра)", correct: "will not be", ending: "at work" },
+    { beginning: "We", bold: "to be (зараз)", correct: "are", ending: "friends" },
 
     //LESSON 3
-    {beginning: "I", bold: "find", correct: "find", ending: "this book interesting"},
-    {beginning: "She", bold: "eat", correct: "eats", ending: "too much sugar"},
-    {beginning: "It", bold: "take", correct: "takes", ending: "a lot of time"},
-    {beginning: "I", bold: "want", correct: "want", ending: "a new phone"},
-    {beginning: "This message", bold: "sound", correct: "sounds", ending: "very important"},
-    {beginning: "My family", bold: "live", correct: "lives", ending: "in the city"},
-    {beginning: "He", bold: "give", correct: "gives", ending: "a good example"},
-    {beginning: "I", bold: "listen", correct: "listen", ending: "to the music"},
+    { beginning: "I", bold: "find", correct: "find", ending: "this book interesting" },
+    { beginning: "She", bold: "eat", correct: "eats", ending: "too much sugar" },
+    { beginning: "It", bold: "take", correct: "takes", ending: "a lot of time" },
+    { beginning: "I", bold: "want", correct: "want", ending: "a new phone" },
+    { beginning: "This message", bold: "sound", correct: "sounds", ending: "very important" },
+    { beginning: "My family", bold: "live", correct: "lives", ending: "in the city" },
+    { beginning: "He", bold: "give", correct: "gives", ending: "a good example" },
+    { beginning: "I", bold: "listen", correct: "listen", ending: "to the music" },
     //LESSON 4
-    
-       
+    { beginning: "You", bold: "make", correct: "will make", ending: "the right decision" },
+    { beginning: "She", bold: "support", correct: "will support", ending: "you" },
+    { beginning: "They", bold: "install", correct: "will install", ending: "a new system" },
+    { beginning: "We", bold: "talk", correct: "will talk", ending: "after the meeting" },
+    { beginning: "He", bold: "buy", correct: "will buy", ending: "a new computer" },
+    { beginning: "I", bold: "do", correct: "will do", ending: "my best to help you" },
+    { beginning: "Your life", bold: "change", correct: "will change", ending: "for the better" },
+    { beginning: "Since you are here, we", bold: "start", correct: "will start", ending: "the new project" },
+
+
 ]
 
 export const FixitExerciseData = sentences.map((value, index) => (
     {
-        id: index+1,
-        lesson: Math.floor(index/8) + 1,
+        id: index + 1,
+        lesson: Math.floor(index / 8) + 1,
         visibility: "hidden",
         input: "",
         sentence: {
--- src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
+++ src/pages/02_myprogress/03_lessonPage/02_grammar/02_exercises/03_fillitin/FillitinExerciseData.js
@@ -24,17 +24,29 @@ const sentences = [
     { sentence: "Сім'я важлива?", translat: "Is family important?" },
     { sentence: "Цей бізнес нелегкий", translat: "This business is not easy" },
     // LESSON 3
-    {sentence: "Він завжди миє свої руки", translat: "He always washes his hands"},
-    {sentence: "Вони допомагають своїм друзям", translat: "They help their friends"},
-    {sentence: "Я ходжу в зал по понеділкам", translat: "I go to the gym on Mondays"},
-    {sentence: "Вона працює в магазині", translat: "She works in the store"},
-    {sentence: "Її син любить гратися", translat: "Her son loves to play"},
-    {sentence: "Мій батько бігає вранці", translat: "My father runs in the mornings"},
-    {sentence: "У цьому місці подають гарну каву", translat: "This place serves good coffee"},
-    {sentence: "Мені потрібна інша ручка", translat: "I need another pen"},
-    {sentence: "Ви мусите подумати про здоров'я", translat: "You must think about your health"},
-    {sentence: "Ми їздимо на тому ж автобусі", translat: "We take the same bus"},
-    
+    { sentence: "Він завжди миє свої руки", translat: "He always washes his hands" },
+    { sentence: "Вони допомагають своїм друзям", translat: "They help their friends" },
+    { sentence: "Я ходжу в зал по понеділкам", translat: "I go to the gym on Mondays" },
+    { sentence: "Вона працює в магазині", translat: "She works in the store" },
+    { sentence: "Її син любить гратися", translat: "Her son loves to play" },
+    { sentence: "Мій батько бігає вранці", translat: "My father runs in the mornings" },
+    { sentence: "У цьому місці подають гарну каву", translat: "This place serves good coffee" },
+    { sentence: "Мені потрібна інша ручка", translat: "I need another pen" },
+    { sentence: "Ви мусите подумати про здоров'я", translat: "You must think about your health" },
+    { sentence: "Ми їздимо на тому ж автобусі", translat: "We take the same bus" },
+    // LESSON 4
+    { sentence: "Ми зараз почнемо збори", translat: "We will start the meeting now" },
+    { sentence: "Вона подвонить тобі на перерві", translat: "She will call you during the break" },
+    { sentence: "Ти багато вивчиш у цьому курсі", translat: "You will learn a lot in this course" },
+    { sentence: "Вони прибудуть біля 6 вечора", translat: "They will arrive around 6p.m." },
+    { sentence: "Джон залишиться на роботі допізна", translat: "John will stay late at work" },
+    { sentence: "Він сформує нову команду", translat: "He will form a new team" },
+    { sentence: "Я приготую м'со на вечерю", translat: "I will cook meat for dinner" },
+    { sentence: "Вона відвідає це місце знову", translat: "She will visit this place again" },
+    { sentence: "Повітря буде відчуватися холоднішим", translat: "The air will feel cooler" },
+    { sentence: "Ми проведемо день разом", translat: "Wt will spend the day together" },
+
+
 
 ]
 
