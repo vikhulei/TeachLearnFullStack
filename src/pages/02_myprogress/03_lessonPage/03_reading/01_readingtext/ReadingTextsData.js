4a530952e63c56a145e4a858c31ef5efea372810
vikhulei <vikhulei@gmail.com>
2025-05-17 18:58:16 +0100

    lesson 10 reading questions completed

--- src/pages/02_myprogress/03_lessonPage/03_reading/01_readingtext/ReadingTextsData.js
+++ src/pages/02_myprogress/03_lessonPage/03_reading/01_readingtext/ReadingTextsData.js
@@ -26,7 +26,7 @@ She woke up early. Her job didn’t start for hours. At home, she liked to read 
     //LESSON 9
     '<b>THE DIGITAL MOVE</b>\nThe web is a useful tool today. Many people develop new ideas online. Every page tells a story. The media industry grows fast. Activity on the internet never stops. One thing is clear: we must move forward. To use technology well is important. It helps us connect and learn. The digital world is always changing. We all take part in this new story.',
     //LESSON 10
-    '<b>THE MAIN FOCUS</b> \nIn any community, safety must matter. Every individual plays a part. The quality of life depends on kind actions. Development happens when we work together. Focus on what helps everyone. Sometimes, everything feels hard to manage. But the main goal is clear: Create a place where all feel safe and valued.',
+    '<b>THE MAIN FOCUS</b> \nIn any community, safety must matter. Every individual plays a part. The quality of life depends on kind actions. Development happens when we work together. That is what helps everyone. Sometimes, everything feels hard to manage. But the main goal is clear: Create a place where everyone feels safe and valued.',
 ]
 
 const images = importAll(
@@ -100,19 +100,19 @@ const rawQuestions = [
     ],
     //lesson 9
     [
-        { question: "What is the man’s1 name?", answer: "Human1" },
-        { question: "What is the man’s2 name?", answer: "Human2" },
-        { question: "What is the man’s3 name?", answer: "Human3" },
-        { question: "Which  symbol was on the map?", answer: "Human4" },
-        { question: "What is the man’s5 name?", answer: "Human5" },
+        { question: "Де люди розробляють нові ідеї?", answer: "online" },
+        { question: "Коли зупиняється активність в інтернеті?", answer: "never" },
+        { question: "Куди потрібно рухатися?", answer: "forward" },
+        { question: "Що допомагає нам робити використання технологій (два слова)?", answer: "connect learn" },
+        { question: "Що постійно робить цифровий світ?", answer: "changing" },
     ],
     //lesson 10
     [
-        { question: "What is the man’s1 name?", answer: "Human1" },
-        { question: "What is the man’s2 name?", answer: "Human2" },
-        { question: "What is the man’s3 name?", answer: "Human3" },
-        { question: "Which  symbol was on the map?", answer: "Human4" },
-        { question: "What is the man’s5 name?", answer: "Human5" },
+        { question: "Що має мати значення в будь-якій спільноті?", answer: "safety" },
+        { question: "Від чого залежить якість життя (два слова)?", answer: "kind actions" },
+        { question: "Як ми повинні працювати для розвитку?", answer: "together" },
+        { question: "Якою є головна ціль?", answer: "clear" },
+        { question: "Як повинні себе почувати всі люди (два слова)?", answer: "safe valued" },
     ],
 ]
 
