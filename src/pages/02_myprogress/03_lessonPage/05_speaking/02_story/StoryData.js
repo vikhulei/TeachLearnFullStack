03e19e422995ac17b96b026dbae03f7debecbba0
vikhulei <vikhulei@gmail.com>
2025-05-30 19:21:23 +0100

    speaking text done for 5 lessons

--- src/pages/02_myprogress/03_lessonPage/05_speaking/02_story/StoryData.js
+++ src/pages/02_myprogress/03_lessonPage/05_speaking/02_story/StoryData.js
@@ -1,16 +1,132 @@
 const stories = [
+
+    // LESSON 1
+    // Introductions & Personal Information
+
     {
         story:
             'Привіт, мене звати Лєна. \nЯ з України. \nЯ приїхала до Великої Британії в лютому 2023р. \nЯ живу в Стратфорді. \nПрацюю в кавярні Coffee #1 в центрі міста. \nЗараз я вивчаю англійську мову. \nМені подобається знайомитися з новими людьми. \nУ вільний час я люблю читати і гуляти вздовж каналу. \nЯ рада бути тут!',
         translation:
             'Hello, my name is Lena. \nI am from Ukraine.\nI arrived in the UK in February 2023. \nI live in Stratford-upon-Avon. \nI work in Coffee #1 in the the center of the town. \nI am learning English now. \nI like meeting new people. \nIn my free time, I enjoy reading, walking along the canal. \nI am happy to be here!'
     },
+
+    // LESSON 2
+    // Daily Routines
+
+    {
+        story:
+            'Я прокидаюся о шостій годині ранку. \nЯ роблю зарядку і приймаю душ. \nПотім я готую сніданок і п’ю каву. \nЯ йду на роботу або працюю вдома. \nВдень я зустрічаюся з колегами і працюю над проектами. \nОбідаю приблизно о дванадцятій годині. \nПісля роботи я ходжу в спортзал або гуляю на вулиці. \nВечеряю з родиною або друзями. \nВечорами я дивлюся новини або читаю книгу. \nЛягаю спати близько десятої години вечора.',
+        translation:
+            'I wake up at six o’clock in the morning. \nI do exercises and take a shower. \nThen I prepare breakfast and drink coffee. \nI go to work or work from home. \nDuring the day, I meet with colleagues and work on projects. \nI have lunch around twelve o’clock. \nAfter work, I go to the gym or take a walk outside. \nI have dinner with my family or friends. \nIn the evenings, I watch the news or read a book. \nI go to bed around ten o’clock at night.'
+    },
+
+
+    // LESSON 3
+    // Hobbies & Free Time
+
 {
     story:
-        'Я прокидаюся о шостій годині ранку. \nЯ роблю зарядку і приймаю душ. \nПотім я готую сніданок і п’ю каву. \nЯ йду на роботу або працюю вдома. \nВдень я зустрічаюся з колегами і працюю над проектами. \nОбідаю приблизно о дванадцятій годині. \nПісля роботи я ходжу в спортзал або гуляю на вулиці. \nВечеряю з родиною або друзями. \nВечорами я дивлюся новини або читаю книгу. \nЛягаю спати близько десятої години вечора.',
+        'У вільний час я люблю малювати. \nЯ також люблю слухати музику. \nІноді я граю на гітарі. \nУ вихідні я часто ходжу на прогулянки. \nМені подобається кататися на велосипеді. \nЩе я люблю читати книжки. \nМій улюблений жанр – детективи. \nЯ часто читаю перед сном. \nМені подобається бути активною. \nХобі допомагають мені розслабитися.',
     translation:
-        'I wake up at six o’clock in the morning. \nI do exercises and take a shower. \nThen I prepare breakfast and drink coffee. \nI go to work or work from home. \nDuring the day, I meet with colleagues and work on projects. \nI have lunch around twelve o’clock. \nAfter work, I go to the gym or take a walk outside. \nI have dinner with my family or friends. \nIn the evenings, I watch the news or read a book. \nI go to bed around ten o’clock at night.'
+        'In my free time, I like to draw. \nI also enjoy listening to music. \nSometimes I play the guitar. \nOn weekends, I often go for walks. \nI like riding my bike. \nI also love reading books. \nMy favorite genre is detective stories. \nI often read before bed. \nI like to stay active. \nHobbies help me relax.'
 },
+
+    // LESSON 4
+    // Travel & Holidays
+
+{
+    story:
+        'Минулого літа я була в Італії. \nЯ відвідала Рим і Венецію. \nМені дуже сподобалась архітектура. \nЯ їла багато піци і морозива. \nПогода була сонячна і тепла. \nЯ зробила багато фотографій. \nЛюди були дуже привітні. \nМоя мрія — поїхати до Японії. \nЯ хочу побачити гору Фудзі. \nПодорожі роблять мене щасливою.',
+    translation:
+        'Last summer, I went to Italy. \nI visited Rome and Venice. \nI really liked the architecture. \nI ate a lot of pizza and ice cream. \nThe weather was sunny and warm. \nI took many photos. \nThe people were very friendly. \nMy dream is to go to Japan. \nI want to see Mount Fuji. \nTraveling makes me happy.'
+},
+
+    // LESSON 5
+    // Food & Cooking
+
+    {
+    story:
+        'Я люблю готувати вдома. \nМоя улюблена страва — борщ. \nЯ готую його з буряком і капустою. \nТакож я люблю варити супи. \nЩе я часто печу пироги. \nЯ люблю готувати для родини. \nМи вечеряємо разом щовечора. \nНа сніданок я їм вівсянку. \nЯ намагаюся їсти здорову їжу. \nГотування — моє улюблене заняття.',
+    translation:
+        'I like cooking at home. \nMy favorite dish is borscht. \nI cook it with beetroot and cabbage. \nI also enjoy making soups. \nI often bake pies too. \nI love cooking for my family. \nWe have dinner together every evening. \nFor breakfast, I eat oatmeal. \nI try to eat healthy food. \nCooking is my favorite activity.'
+},
+
+    // LESSON 6
+    // Family & Friends
+
+    // LESSON 7
+    // School & Education
+
+    // LESSON 8
+    // Work & Careers
+
+    // LESSON 9
+    // Shopping & Money
+
+    // LESSON 10
+    // Technology & Social Media
+
+    // LESSON 11
+    // Movies & TV Shows
+
+    // LESSON 12
+    // Music & Entertainment
+
+    // LESSON 13
+    // Sports & Exercise
+
+    // LESSON 14
+    // News & Current Events
+
+    // LESSON 15
+    // Dreams & Future Plans
+
+    // LESSON 16
+    // Cultural Differences
+
+    // LESSON 17
+    // Weather & Seasons
+
+    // LESSON 18
+    // Health & Wellness
+
+    // LESSON 19
+    // Personal Experiences & Stories
+
+    // LESSON 20
+    // If You Could...
+
+    // LESSON 21
+    // Books & Reading
+
+    // LESSON 22
+    // Pets & Animals
+
+    // LESSON 23
+    // Environment & Nature
+
+    // LESSON 24
+    // Hometown & Neighborhood
+
+    // LESSON 25
+    // Languages & Communication
+
+    // LESSON 26
+    // Fashion & Style
+
+    // LESSON 27
+    // Celebrations & Festivals
+
+    // LESSON 28
+    // Art & Creativity
+
+    // LESSON 29
+    // Transportation & Commuting
+
+    // LESSON 30
+    // Challenges & Achievements
+
+
     {
         story:
             'ЦЕ ІСТОРІЯ №13! Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож.. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож.. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож.. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож.. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож.. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож. Так! Але щоб дізнатися більше, нам потрібно взяти (take) цю карту та вирушити в подорож',
@@ -65,8 +181,8 @@ const stories = [
 
 export const StoryData = stories.map((value, index) => (
     {
-        id: index+1,
-        lesson: index+1,
+        id: index + 1,
+        lesson: index + 1,
         story: value.story,
         translation: value.translation,
         display: "none"
