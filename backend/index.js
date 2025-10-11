e1811d5bf513de5fae1e6289957ca10d4e904b11
vikhulei <vikhulei@gmail.com>
2025-10-11 13:48:15 +0100

    middleware added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
  const express = require("express")
  const app = express()
+ const cors = require("cors")
  const PORT = "5000"
+
+ app.use(cors())
+ app.use(express.json())
+
 
  app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
