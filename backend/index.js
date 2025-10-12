9c9d9ae76a37b98503d4556632977f2967a8e631
vikhulei <vikhulei@gmail.com>
2025-10-12 17:39:49 +0100

    started working on connecting db with the server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
  const express = require("express")
  const app = express()
  const cors = require("cors")
+ const pool = require("./db")
  const PORT = "5000"
 
  app.use(cors())
- app.use(express.json())
+ app.use(express.json()); //req.body
+
+ //ROUTES
+
+ //create a todo
+ app.post("/todos", (req, res) => {
+   try {
+      
+   } catch (err) {
+      console.error(err.message);
+   }
+ })
+
+ //get all todos
+
+ //update a todo
+
+ //delete a todo
 
 
  app.listen(PORT, () => {
