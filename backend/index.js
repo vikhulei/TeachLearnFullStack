ed178ff99c834dd27e292b1bf7fbc892aa4e475c
vikhulei <vikhulei@gmail.com>
2025-10-13 19:08:44 +0100

    get a todo completed

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,42 @@
  //ROUTES
 
  //create a todo
- app.post("/todos", (req, res) => {
+ app.post("/todos", async (req, res) => {
    try {
-      
+      const { description } = req.body;
+      const newTodo = await pool.query(
+         "INSERT INTO todo (description) VALUES($1) RETURNING *",
+         [description]
+      );
+      res.json(newTodo.rows[0])
    } catch (err) {
       console.error(err.message);
    }
  })
 
  //get all todos
+
+ app.get("/todos", async(req, res) => {
+   try {
+      const allTodos = await pool.query("SELECT * FROM todo")
+      res.json(allTodos.rows)
+   } catch (err) {
+      console.error(err.message)
+   }
+ })
+
+ //get a todo
+app.get("/todos/:id", async(req, res) => {
+   try {
+      const { id } = req.params;
+      const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+
+      res.json(todo.rows)
+   } catch (err) {
+      console.error(err.message)
+   }
+})
+
 
  //update a todo
 
